0be52079ce252fa535fc220450237c84f4fb710c
Joshua Chadwick <jchadwick423@gmail.com>
2023-04-15 01:09:07 -0700

    Properly Configured Project
    Modified the project properties (C/C++ & Linker properties) to properly detect and link SFML libraries on any cloned repo (hopefully). Added basic circle demo code to main.cpp. Added SFML library locally to this repo.

--- PA9/PA9/main.cpp
+++ PA9/PA9/main.cpp
@@ -1,15 +1,38 @@
+/*
+* Collaborators: Simon Meili, Zack Felcyn, Joshua Chadwick, Haley Boileau
+* Project Name: PA 9 - Anime Date Simulator
+* Due Date: April 26, 2023
+* Professor: Andy O'Fallon
+* 
+* Description: N/A
+*/
 
+// NOTE: When compiling and running this program, ensure that the SFML DLLs are in the same directory as the output build!
+// Ex. ./PA9/x64/Debug
+// 
+// Additional Note: SFML only allows compiling and executing in machine type x64, so x86 & arm64 won't compile to my knowledge...
 
+#include <SFML/Graphics.hpp>
 
-
-int main(void)
+int main()
 {
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 
-
-
-
-
-	return 0;
+    return 0;
 }
