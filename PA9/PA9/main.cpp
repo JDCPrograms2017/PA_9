db031afb8a793c00340977701b4221d7cac21567
nokuree <meilisimon378@gmail.com>
2023-04-18 17:02:40 -0700

    has the template for the game

--- PA9/PA9/main.cpp
+++ PA9/PA9/main.cpp
@@ -17,11 +17,21 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(735, 413), "wooha");
+
     sf::RectangleShape background;
     background.setSize(sf::Vector2f(735, 413));
     sf::Texture Maintexture;
     Maintexture.loadFromFile("Textures/funny.jpg");
     background.setTexture(&Maintexture);
+
+    sf::RectangleShape rec_shape;
+    rec_shape.setSize(sf::Vector2f(735, 413));
+    sf::Texture text_Texture;
+    text_Texture.loadFromFile("Textures/text_box.png");
+    rec_shape.setTexture(&text_Texture);
+    rec_shape.setPosition(0, 100);
+
+
 
     while (window.isOpen())
     {
@@ -34,6 +44,7 @@ int main()
 
         window.clear();
         window.draw(background);
+        window.draw(rec_shape);
         window.display();
     }
 
