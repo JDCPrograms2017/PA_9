d27848f23433a1516dad71d6f3b1f4903e7c8bf8
zfelcyn <zachary.felcyn@wsu.edu>
2023-04-25 12:32:00 -0700

    Fixes Github glitch, it keeps formatting the Button.hpp wrong ish?
    Josh encountered error too, github uploaded one line wrong which doesnt make sense. Oh well question for the gods

--- PA9/PA9/Button.hpp
+++ PA9/PA9/Button.hpp
@@ -7,17 +7,13 @@
 class Button : public sf::RectangleShape {
 
 public:
-	Button(const sf::Vector2f& newSize = sf::Vector2f(0, 0), 
-		   const sf::Vector2f& newPos = sf::Vector2f(0, 0), 
-		   const sf::Texture& newButtonTexture = sf::Texture(), 
+	Button(const sf::Vector2f& newSize = sf::Vector2f(0, 0),
+		const sf::Vector2f& newPos = sf::Vector2f(0, 0),
+		const sf::Texture& newButtonTexture = sf::Texture(),
 		std::string insideText = "No text") : sf::RectangleShape(newSize), position(newPos), buttonTexture(newButtonTexture) {
 
 		toggle = false;
-);
-
 		dynamic_cast<sf::RectangleShape&>(*this).setTexture(&buttonTexture);
-
-
 		std::cout << "New button created!\n";
 		this->setPosition(position);
 
@@ -26,7 +22,7 @@ public:
 	};
 
 	void toggleButton();
-	void setPosition(sf::Vector2f &newPosition);
+	void setPosition(sf::Vector2f& newPosition);
 	void changeButtonTexture(sf::Texture& newTexture);
 	void setButtonText(std::string newText) {
 		buttonText.setString(newText);
@@ -50,10 +46,7 @@ public:
 	}
 
 	bool getButtonState();
-
-	bool isBeingPushed(const sf::Window &windowRef);
-
-
+	bool isBeingPushed(const sf::Window& windowRef);
 	sf::RectangleShape& getDrawableShape();
 private:
 	sf::Vector2f position;
